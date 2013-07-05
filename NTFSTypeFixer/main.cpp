2704eb0f55da740052b434762563da603e9fe75f
Sun Junwen <sunjw@org-sunjw-mbp.local>
2013-07-05 20:41:19 +0800

    Initial Commit

---
+++ NTFSTypeFixer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  NTFSTypeFixer
+//
+//  Created by Sun Junwen on 13-7-5.
+//  Copyright (c) 2013年 Sun Junwen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
